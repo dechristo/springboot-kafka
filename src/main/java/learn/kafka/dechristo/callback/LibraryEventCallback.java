91eb8b4faba521a76f15ba3fb9c97e01a71954de
luizeduardo <dudu.christo@gmail.com>
2022-10-14 14:33:53 +0200

    First commit

--- src/main/java/learn/kafka/dechristo/callback/LibraryEventCallback.java
+++ src/main/java/learn/kafka/dechristo/callback/LibraryEventCallback.java
@@ -1,7 +1,9 @@
 package learn.kafka.dechristo.callback;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+@Component
 public class LibraryEventCallback implements ListenableFutureCallback {
     @Override
     public void onFailure(Throwable ex) {
